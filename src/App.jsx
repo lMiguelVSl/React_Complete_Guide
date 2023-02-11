13fb8146ebff5cf795d2c0ab4d2f8c76df045ad4
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-11 10:35:12 -0500

    Using new component test

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import TestComponent from "./TestComponent";
+
+function App() {
+  return (
+    <div className="App">
+      Let's get started
+      <TestComponent></TestComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TestComponent.jsx
@@ -0,0 +1,7 @@
+function TestComponent() {
+    return (
+        <h1>Miguel Vargas</h1>
+    );
+}
+
+export default TestComponent;
